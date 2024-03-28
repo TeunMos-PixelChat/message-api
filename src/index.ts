d198db020a9b1c1caaef2b8de7fdba4477bf0e9e
teunmos <teun.mos@gmail.com>
2024-03-28 12:44:45 +0100

    azure deployment

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.post("/test", (req: Request, res: Response) => {
   res.json({
     message: "Hello, world!",
     echo: body,
-    meta: "This is a test endpoint."
+    meta: "This is a test endpoint. blabla"
   });
 });
 
