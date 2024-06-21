ff5a99870e2e6bc690def61ab804331fa50ae0a9
teunmos <teun.mos@gmail.com>
2024-06-21 15:25:38 +0200

    fixed existing tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
+process.env.RABBITMQ_HOST = "localhost";
+process.env.RABBITMQ_USER = "user";
+process.env.RABBITMQ_PASS = "pass";
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
--- src/__tests__/testEndpoints.ts
+++ src/__tests__/testEndpoints.ts
@@ -1,14 +1,44 @@
 import supertest from "supertest"; // supertest is a framework that allows to easily test web APIs
+import { ArrayOrObject, QueryOptions } from "cassandra-driver";
+import { DirectChatMessage } from "../typings/cassandraModels";
+import { randomUUID } from "crypto";
 import { app, server } from "../index";
 
 // it("Testing to see if Jest works", () => {
 //   expect(2).toBe(2);
 // });
 
+// Mock the "cassandra-driver" package
+jest.mock("cassandra-driver", () => ({
+  Client: jest.fn().mockImplementation(() => ({
+    execute: (query: string, params?: ArrayOrObject | undefined, options?: QueryOptions | undefined) => {
+      return {
+        rows: [
+          { id: randomUUID(), sender_id: "1", receiver_id: "2", message: "Hello, world!", created_at: new Date() },
+          { id: randomUUID(), sender_id: "2", receiver_id: "1", message: "Hello, world!", created_at: new Date() },
+        ],
+      };
+    },
+  })),
+}));
+
+jest.mock("amqplib", () => ({
+  connect: jest.fn().mockImplementation(() => ({
+    createChannel: jest.fn().mockImplementation(() => ({
+      assertQueue: jest.fn().mockImplementation(() => ({
+        sendToQueue: jest.fn(),
+      })),
+      consume: jest.fn(),
+      ack: jest.fn(),
+    })),
+  })),
+}));
+
+
 
 it("Root Endpoint /test", async () => {
   const response = await supertest(app).get("/");
-  expect(response.text).toBe("Hello, world! Development");
+  expect(response.text).toContain("Hello, world!");
 });
 
 
